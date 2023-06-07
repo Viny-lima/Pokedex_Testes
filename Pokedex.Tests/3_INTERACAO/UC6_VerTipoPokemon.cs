6afba3c4412dfa25bc6c814c5a90e0a710f6eeca
lucasi <hurryblank@gmail.com>
2023-06-07 12:17:17 -0300

    3 interacao

---
+++ Pokedex.Tests/3_INTERACAO/UC6_VerTipoPokemon.cs
@@ -0,0 +1,33 @@
+﻿using Pokedex.Model.Enums;
+
+using Pokedex.Model.Service;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokedex.Tests._3_INTERACAO
+{
+    [Collection("Database")]
+    public class UC6_VerTipoPokemon
+    {
+        [Theory(DisplayName ="Buscar os tipos do pokemon de acordo com o nome dele")]
+        [InlineData("pikachu", TypeNames.electric)]
+        [InlineData("charmander", TypeNames.fire)]
+        public void BuscarTiposPokemon(string nome, TypeNames tipo)
+        {
+            var name = nome;
+            var service = new PokemonService();
+
+            var valorObtido = service.FindByName(name).Result.Types;
+
+            var valorEsperado = tipo;
+
+            Assert.Contains(valorObtido, p => p.Type.Name == valorEsperado.ToString());
+        }
+    }
+
+}
---
+++ Pokedex.Tests/3_INTERACAO/UC7_VerHabilidadePokemon.cs
@@ -0,0 +1,31 @@
+﻿using Pokedex.Model.Service;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokedex.Tests._3_INTERACAO
+{
+    [Collection("Database")]
+    public class UC7_VerHabilidadePokemon
+    {
+        [Theory(DisplayName = "Ver habilidades do pokemon pelo nome fornecido")]
+        [InlineData("pikachu", "lightning-rod", "static")]
+        [InlineData("charmander", "blaze", "solar-p")]
+        [InlineData("metapod", "shed-skin")]
+        [InlineData("golbat", "inner-focus", "infiltrator")]
+        public void VerHabilidadePokemon(string nome, params string[] skills)
+        {
+            var name = nome;
+            var service = new PokemonService();
+            var skill = skills;
+
+            var pokemon = service.FindByName(name).Result.Abilities;
+
+            Assert.Contains(pokemon, p => skills.Contains(p.Ability.Name.ToString()));
+        }
+    }
+}
