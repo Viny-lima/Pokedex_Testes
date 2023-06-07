b8316cb1f5046c87045b3c6b122fcd2866a83d51
lucasi <hurryblank@gmail.com>
2023-06-07 17:52:57 -0300

    mais iteracoes

--- Pokedex.Tests/3_INTERACAO/UC6_VerTipoPokemon.cs
+++ Pokedex.Tests/3_INTERACAO/UC6_VerTipoPokemon.cs
@@ -16,7 +16,25 @@ namespace Pokedex.Tests._3_INTERACAO
     {
         [Theory(DisplayName ="Buscar os tipos do pokemon de acordo com o nome dele")]
         [InlineData("pikachu", TypeNames.electric)]
+        [InlineData("squirtle", TypeNames.water)]
+        [InlineData("articuno", TypeNames.ice)]
+        [InlineData("mewtwo", TypeNames.psychic)]
+        [InlineData("dragonite", TypeNames.dragon)]
+        [InlineData("lapras", TypeNames.water)]
         [InlineData("charmander", TypeNames.fire)]
+        [InlineData("bulbasaur", TypeNames.grass)]
+        [InlineData("jigglypuff", TypeNames.normal)]
+        [InlineData("meowth", TypeNames.normal)]
+        [InlineData("psyduck", TypeNames.water)]
+        [InlineData("snorlax", TypeNames.normal)]
+        [InlineData("eevee", TypeNames.normal)]
+        [InlineData("gengar", TypeNames.ghost)]
+        [InlineData("machamp", TypeNames.fighting)]
+        [InlineData("vaporeon", TypeNames.water)]
+        [InlineData("flareon", TypeNames.fire)]
+        [InlineData("jolteon", TypeNames.electric)]
+        [InlineData("mew", TypeNames.psychic)]
+        [InlineData("zapdos", TypeNames.electric)]
         public void BuscarTiposPokemon(string nome, TypeNames tipo)
         {
             var name = nome;
