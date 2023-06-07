99c175a521b0cb8fb0440ac587c4ea3f2083492e
Viny Lima <vfl.computer@gmail.com>
2023-06-07 18:37:17 -0300

    INTERACAO 2

---
+++ Pokedex.Tests/2_INTERACAO/UC4_CadastrarPokemon.cs
@@ -0,0 +1,87 @@
+﻿using Pokedex.Model.Entities;
+using Pokedex.Model.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokedex.Tests._2_INTERACAO
+{
+    [Collection("Database")]
+    public class UC4_CadastrarPokemon
+    {
+        [Fact]
+        public async void CadastrandoNovoPokemonComDadosValidos()
+        {
+
+            var pokemon = new PokemonDB()
+            {
+                Name = "PokemonTest",
+                Hp = 100,
+                Attack = 100,
+                Defense = 100,
+                Height = 100,
+                SpecialAttack = 100,
+                SpecialDefense = 100,
+                Speed = 100,
+                BaseExperience = 100,
+                IsComplete = true,
+                IsCreatedByTheUser = true,
+            };
+
+            await pokemon.AddAbility("Ability Test");
+            await pokemon.AddMove("Move Test");
+            await pokemon.AddType("Type1 Test");
+            await pokemon.AddType("Type2 Test");
+
+            await pokemon.SetId();
+
+            //Act
+            await new PokemonService().RegisterIsCreatedByUser(pokemon);
+
+            //Assert
+            var pokemonObtido = await new PokemonService().FindById(pokemon.Id);
+            var pokemonEsperado = pokemon;
+
+            Assert.Equal(pokemonEsperado, pokemonObtido);
+        }
+
+        [Fact]
+        public async void CadastrandoNovoPokemonComDadosInvalidos()
+        {
+            var pokemon = new PokemonDB()
+            {
+                // Dados inválidos
+                Name = null, // Nome nulo
+                Hp = -10, // HP negativo
+                Attack = 0, // Ataque zero
+                Defense = 2000, // Defesa alta demais
+                Height = -1, // Altura negativa
+                SpecialAttack = 50,
+                SpecialDefense = 50,
+                Speed = 50,
+                BaseExperience = 50,
+                IsComplete = true,
+                IsCreatedByTheUser = true,
+            };
+
+            await pokemon.AddAbility("Ability Test");
+            await pokemon.AddMove("Move Test");
+            await pokemon.AddType("Type1 Test");
+            await pokemon.AddType("Type2 Test");
+
+            await pokemon.SetId();
+
+            // Act
+            await new PokemonService().RegisterIsCreatedByUser(pokemon);
+
+            // Assert
+            var pokemonObtido = await new PokemonService().FindById(pokemon.Id);
+
+            // Verifique se o Pokémon obtido é nulo, indicando que não foi registrado corretamente
+            Assert.Null(pokemonObtido);
+        }
+    }
+}
---
+++ Pokedex.Tests/2_INTERACAO/UC5_ListaOrdenada.cs
@@ -0,0 +1,71 @@
+﻿using Pokedex.Model.Service;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokedex.Tests._2_INTERACAO
+{
+    [Collection("Database")]
+    public class UC5_ListaOrdenada
+    {
+
+        [Fact]
+        public async void VerificarListaDePokemonsEmOrdem()
+        {
+            //Arrange
+            var service = new PokemonService();
+            var _inicio = 1;
+            var _quantidade = 890;
+
+            //Act
+            var listaDePokemons = await service.FindAllById(_inicio, _quantidade);
+            var IDs = new List<int>();
+
+            foreach (var pokemon in listaDePokemons)
+            {
+                IDs.Add(pokemon.Id);
+            }
+
+            //Assert
+            Assert.True(AreIdsOrdered(IDs));
+        }
+
+        public bool AreIdsOrdered(List<int> ids)
+        {
+            for (int i = 0; i < ids.Count - 1; i++)
+            {
+                if (ids[i] > ids[i + 1])
+                {
+                    // IDs estão fora de ordem crescente
+                    return false;
+                }
+            }
+
+            // IDs estão em ordem crescente ou vazios
+            return true;
+        }
+
+        [Theory]
+        [InlineData(1, 10)]
+        [InlineData(5, 7)]
+        [InlineData(890, 10)]
+        public async void VerificarQuatidadeEspecificada(int inicio, int quantidade)
+        {
+            //Arrange
+            var service = new PokemonService();
+            var _quantidade = quantidade;
+            var _inicio = inicio;
+
+            //Act
+            var resultado = await service.FindAllById(_inicio, _quantidade);
+
+            //Assert
+            Assert.Equal(_quantidade, resultado.Count);
+        }
+
+    }
+}
